fdc69bec3f80a51097d8bd67fb50fd6a95766804
mxdi9i7 <peter.zheng88228@gmail.com>
2021-09-18 20:55:07 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const POKEMONS_PER_PAGE = 20;
 
 function App() {
+  const [data, setData] = useState(null);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const offset = (page - 1) * POKEMONS_PER_PAGE;
+    fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=${POKEMONS_PER_PAGE}&offset=${offset}`,
+    )
+      .then((resp) => resp.json())
+      .then((result) => {
+        setData(result.results);
+        setLoading(false);
+      });
+  }, [page]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div className='container'>
+        {data ? (
+          data.map((v, i) => {
+            return (
+              <div className='pokemon button'>
+                <p>{v.name}</p>
+              </div>
+            );
+          })
+        ) : (
+          <p>No Pokemons</p>
+        )}
+      </div>
+      <div className='pagination'>
+        <button
+          disabled={page === 1 || loading}
+          className='button'
+          onClick={() => setPage(page === 1 ? 1 : page - 1)}
         >
-          Learn React
-        </a>
-      </header>
+          Prev
+        </button>
+        <button
+          disabled={loading}
+          className='button'
+          onClick={() => setPage(page + 1)}
+        >
+          Next
+        </button>
+      </div>
+      <p className='page-number'>Page {page}</p>
     </div>
   );
 }
